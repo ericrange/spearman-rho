8a19713eb72f0d59aa89830e0e9714ebb847c2ec
Eric Range <mail@eric-range.de>
2016-05-01 14:17:24 +0200

    Update

--- test.js
+++ test.js
@@ -2,5 +2,9 @@
 
 const SpearmanRHO = require("./spearman-rho");
 
-let spearmanRHO = new SpearmanRHO([2.0, 3.0, 3.0, 5.0, 5.5, 8.0, 10.0, 10.0], [1.5, 1.5, 4.0, 3.0, 1.0, 5.0, 5.0, 9.5]);
+const X = [2.0, 3.0, 3.0, 5.0, 5.5, 8.0, 10.0, 10.0];
+const Y = [1.5, 1.5, 4.0, 3.0, 1.0, 5.0, 5.0, 9.5];
+
+const spearmanRHO = new SpearmanRHO(X, Y);
 console.log(spearmanRHO.calc());
+// 0.6829268292682927
